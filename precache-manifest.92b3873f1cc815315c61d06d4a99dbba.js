ec87d0b6df2687905ce35640f263931c866da655
Anandmalani <malanianand2000@gmail.com>
2022-10-06 23:46:19 +0530

    Updates

---
+++ precache-manifest.92b3873f1cc815315c61d06d4a99dbba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "60f000876d763df59537",
+    "url": "/TestUtils-React-App/static/css/main.cab3f056.chunk.css"
+  },
+  {
+    "revision": "60f000876d763df59537",
+    "url": "/TestUtils-React-App/static/js/main.60f00087.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/TestUtils-React-App/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "80ab108f695beb50134e",
+    "url": "/TestUtils-React-App/static/js/2.80ab108f.chunk.js"
+  },
+  {
+    "revision": "d3d3d0c9de5a5edfd8da",
+    "url": "/TestUtils-React-App/static/js/runtime~main.d3d3d0c9.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TestUtils-React-App/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7ff04a0a694416117432f2362739c877",
+    "url": "/TestUtils-React-App/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/TestUtils-React-App/precache-manifest.92b3873f1cc815315c61d06d4a99dbba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/TestUtils-React-App/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
